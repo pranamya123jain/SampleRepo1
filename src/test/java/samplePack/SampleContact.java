b48eef48002e48c23f3ec1b2afc901ca98db70c7
pranamya123jain <pranamya123jain@gmail.com>
2024-11-13 17:17:06 +0530

    commit

---
+++ src/test/java/samplePack/SampleContact.java
@@ -0,0 +1,17 @@
+package samplePack;
+
+import org.testng.annotations.Test;
+
+public class SampleContact {
+	@Test
+	public void sampleDemo()
+	{
+		System.out.println("====");
+	}
+	@Test
+	public void sampleDemo1()
+	{
+		System.out.println("=+++++");
+	}
+
+}
---
+++ src/test/java/samplePack2/SampleOrg.java
@@ -0,0 +1,20 @@
+package samplePack2;
+
+import org.testng.annotations.Test;
+
+public class SampleOrg {
+		@Test
+		public void sampleDemo2()
+		{
+			System.out.println("=+++=");
+		}
+
+		@Test
+		public void sampleDemo3()
+		{
+			System.out.println("=+++=");
+		}
+	}
+
+
+
